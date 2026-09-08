ba1dc26caee897a1a39f4ce8c909761bf90ebd52
Pyromesis <141385602+Pyromesis@users.noreply.github.com>
2026-09-08 16:28:53 -0500

    Release 2.1.10: tooltips en Solucionar + fix filtro Activos + mejoras UI

--- src/CA-O.Shared/Constants/BuildConstants.cs
+++ src/CA-O.Shared/Constants/BuildConstants.cs
@@ -11,10 +11,10 @@ namespace CAO.Shared.Constants;
 public static class BuildConstants
 {
     // Version - SINGLE SOURCE OF TRUTH
-    public const string ProductVersion = "2.1.9";
+    public const string ProductVersion = "2.1.10";
     public const string ProductVersionMajor = "2";
     public const string ProductVersionMinor = "1";
-    public const string ProductVersionPatch = "9";
+    public const string ProductVersionPatch = "10";
 
     // Product Identity
     public const string ProductName = "CA-O";
